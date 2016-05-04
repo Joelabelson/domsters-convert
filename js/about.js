dd5f7b1d1e9b4321475cb985318fed914245b88d
Joel Abelson <joel.abelson@me.com>
2016-05-04 13:42:27 -0700

    remove normalize.css

--- js/about.js
+++ js/about.js
@@ -1,3 +1,20 @@
+// $(document).ready(function (){
+  // console.log('ready');
+// });
+
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
 function showSection(id) {
   var divs = document.getElementsByTagName("div");
   for (var i=0; i<divs.length; i++ ) {
@@ -10,22 +27,23 @@ function showSection(id) {
   }
 }
 
-function prepareInternalnav() {
-  if (!document.getElementsByTagName) return false;
-  if (!document.getElementById) return false;
-  if (!document.getElementById("internalnav")) return false;
-  var nav = document.getElementById("internalnav");
-  var links = nav.getElementsByTagName("a");
-  for (var i=0; i<links.length; i++ ) {
-    var sectionId = links[i].getAttribute("href").split("#")[1];
-    if (!document.getElementById(sectionId)) continue;
-    document.getElementById(sectionId).style.display = "none";
-    links[i].destination = sectionId;
-    links[i].onclick = function() {
-      showSection(this.destination);
-      return false;
-    }
-  }
+function prepareInternalNav() {
+  console.log('hello');
+//   if (!document.getElementsByTagName) return false;
+//   if (!document.getElementById) return false;
+//   if (!document.getElementById("internalnav")) return false;
+//   var nav = document.getElementById("internalnav");
+//   var links = nav.getElementsByTagName("a");
+//   for (var i=0; i<links.length; i++ ) {
+//     var sectionId = links[i].getAttribute("href").split("#")[1];
+//     if (!document.getElementById(sectionId)) continue;
+//     document.getElementById(sectionId).style.display = "none";
+//     links[i].destination = sectionId;
+//     links[i].onclick = function() {
+//       showSection(this.destination);
+//       return false;
+//     }
+//   }
 }
 
-addLoadEvent(prepareInternalnav);
+addLoadEvent(prepareInternalNav);
